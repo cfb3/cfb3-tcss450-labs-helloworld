352f542f9e1f4ecb30dcd32e89603ff8e1057867
charles <cfb3@uw.edu>
2022-06-16 14:17:35 -0700

    pushme: added testing pfms endpoint

--- routes/pfms.js
+++ routes/pfms.js
@@ -8,6 +8,13 @@ const logger = require('cfb3-log-package')
 
 const PFMS = require('pfms')
 
+const algorithmMap = new Map()
+algorithmMap.set('Average Strength', PFMS.genClsAvgEdgeWeight)
+algorithmMap.set('Total Strength', PFMS.genClsTotalEdgeWeight)
+algorithmMap.set('Centrality via Edge Weight x Left Eigenvector', PFMS.genClsAvgEdgeWeightProdPageRank)
+algorithmMap.set('Centrality via Edge Weight x Right Eigenvector', PFMS.genClsAvgEdgeWeightProdEigen)
+algorithmMap.set('Centrality via Right Eigenvector (no Edge Weight)', PFMS.genClsAvgVertexEigen)
+
 /**
  * @api {get} /hello Request a Hello World message
  * @apiName GetHello
@@ -18,13 +25,35 @@ const PFMS = require('pfms')
 router.get("/", (request, response) => {
 
     const matrix = request.body.matrix
+    const factorNames = request.body.factorNames
+    const factor = request.body.factor
+    const maxLoopLength = request.body.maxLoopLength
+    const algorithm = algorithmMap.get(request.body.algorithmName)(factorNames, matrix)
+    const maxLoopCount = request.body.maxLoopCount
     
-    let sum = PFMS.sum(matrix)
+    PFMS.uniqueFBLoops(
+            matrix,
+            factorNames,
+            factor,
+            loopLength,
+            maxLoopLength,
+            algorithm,
+            maxLoopCount)
+        .then(result => {
+            response.set('Access-Control-Allow-Origin', '*')
+            response.send({
+                message: `The sum is ${sum}`
+            })
+        })
+        .catch(error => {
+            response.set('Access-Control-Allow-Origin', '*')
+            response.send({
+                error: error
+            })
+        })
 
-    response.set('Access-Control-Allow-Origin', '*')
-    response.send({
-        message: `The sum is ${sum}`
-    })
+
+
 })
 
 router.post("/", (request, response) => {
