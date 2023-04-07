f67882e3a4a5b2307ed23134eebfa3e69263183f
charles <cfb3@uw.edu>
2023-04-07 16:13:48 -0700

    pushme: trying to connect

--- utilities/sql_conn.js
+++ utilities/sql_conn.js
@@ -1,10 +1,25 @@
-// Obtain a Pool of DB connections. 
-const { Pool } = require('pg')
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    }
-})
+// Obtain a Pool of DB connections.
+const pg = require("pg");
+const config = {
+  host: "450test.postgres.database.azure.comm",
+  // Do not hard code your username and password.
+  // Consider using Node environment variables.
+  user: "cfb3",
+  password: "Tnvrno11",
+  database: "sample",
+  port: 5432,
+  ssl: true,
+};
 
-module.exports = pool
+const client = new pg.Client(config);
+
+client.connect((err) => {
+  if (err) throw err;
+  else {
+    console.log("Connected");
+  }
+});
+
+const pool = client;
+
+module.exports = pool;
